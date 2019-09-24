b56c4d9f0b2ad9bb8f72965c0037c95dd66e3b91
lauraturner <lauraturner512@hotmail.com>
2019-09-24 00:28:15 -0400

    Finalize algorithm for flight data

--- Dijkstra_Algorithm.py
+++ Dijkstra_Algorithm.py
@@ -1,47 +1,70 @@
 import pandas as pd
 import numpy as np
 
-def find_min(remaining, dist):
+# find the minimum current arrival time of the remaining locations
+def find_min(remaining, arrival):
     minimum = float('inf')
     min_indx = None
     for v in remaining: 
-        if dist[v] < minimum:
-            minimum = dist[v]
+        if arrival[v] < minimum:
+            minimum = arrival[v]
             min_indx = v
     return min_indx
 
-start = 0
-FILE_NAME = 'Dijkstra_Data_1600.txt'
-num_vert = pd.read_csv(FILE_NAME, nrows=1, header=None).iat[0,0]
-graph = pd.read_csv(FILE_NAME, skiprows=1, header=None, sep='\t')
-graph = graph.drop(graph.columns[num_vert], axis=1)
-dist = [None] * num_vert
-prev = [None] * num_vert
-remaining = []
+# find the path of the shortest flight to the destination
+def get_path(prev, end):
+    path = []
+    i = end
+    path.append(end)
+    while prev[i] != None:
+        path.append(prev[i])
+        i = prev[i]
+    return path
 
-for v in graph.index:
-    dist[v] = float('inf')
-    prev[v] = None
-    remaining.append(int(v))
+# print the results of the algorithm
+def print_results(path, start, end, arrival):
+    print('Optimal route from {} to {} \n'.format(start, end))
+    for i in range(len(path) - 1, 0, -1):
+        print('Fly from {} to {}'.format(path[i] , path[i - 1]))
+    print('\nArrive at {} at time {}'.format(end, arrival[end]))
 
-print(graph.index)
+# Main algorithm to find the shortest flight
+def shortest_flight(FILE_NAME, start, end):
 
-dist[start] = 0
+    # get data from the text file
+    locs = pd.read_csv(FILE_NAME, nrows=1, header=None).iat[0,0]
+    graph = pd.read_csv(FILE_NAME, skiprows=1, header=None, sep='\t')
 
-while len(remaining) > 0:
-    current = find_min(remaining, dist)
-    remaining.remove(current)
-    graph = graph.drop(current, axis=1)
-    for i in graph.columns:
-        if graph.at[current, i] == 0:
-            continue
-        est = dist[current] + graph.at[current, i]
-        if est < dist[i]:
-            dist[i] = est
-            prev[i] = current
+    # set up variables for the algorithm
+    arrival = [None] * locs
+    prev = [None] * locs
+    remaining = []
+    for v in range(0, locs):
+        arrival[v] = float('inf')
+        prev[v] = None
+        remaining.append(int(v))
+    arrival[start] = 0
 
+    while len(remaining) > 0:
+        current = find_min(remaining, arrival)
+        #break if the shortest path has already been found 
+        if current == end:
+            break
+        remaining.remove(current)
+        for _, row in graph.loc[graph[0] == current].iterrows():
+            if row[3] < arrival[row[1]] and arrival[current] < row[2]:
+                arrival[row[1]] = row[3]
+                prev[row[1]] = current
 
+    # check to see if a path was found to the destination
+    if arrival[end] == float('inf'):
+        print('There is no path from {} to {}'.format(start, end))
+    else:
+        path = get_path(prev, end)
+        print_results(path, start, end, arrival)
 
-
-
-
+# Initial parameters
+start = 99
+end = 87
+FILE_NAME = '2019_Lab_2_flights_real_data.txt'
+shortest_flight(FILE_NAME, start, end)
